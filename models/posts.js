52223a91c6266f045ef1acd96a132d773cdfb7e6
wuzhihao <wuzhihao1996727@qq.com>
2017-08-28 16:57:00 +0800

    完成文章类

---
+++ models/posts.js
@@ -0,0 +1,60 @@
+// 发表文章model
+var Post = require('../lib/mongo').Post;
+// 引入markdown插件
+var marked = require('marked');
+
+// 将markdown转换成html
+// 不是很懂这里....
+Post.plugin('contentToHtml', {
+	afterFind: function (posts) {
+		return posts.map(function (post) {
+			post.content = marked(post.content);
+			return post;
+		});
+	},
+	afterFindOne: function (post) {
+		if (post) {
+			post.content = marked(post.content);
+		}
+		return post;
+	}
+})
+module.exports = {
+	// 新建文章
+	create: function create(post) {
+		return Post.create(post).exec()
+	},
+
+	// 根据ID获取文章
+	// 不是很懂...
+	getPostById: function getPostById(postId) {
+		return Post
+			.findOne({_id: postId})
+			.populate({path: 'author', model: 'User'})
+			.addCreatedAt()
+			.contentToHtml()
+			.exec();
+	}，
+
+	// 根据创建时间降序获取素有用户文章或者某个特定用户的所有文章
+	getPosts: function getPosts(author) {
+		var query = {};
+		if (author) {
+			query.author = author;
+		}
+		return Post
+			.find(query)
+			.populate({path: 'author', model: 'User'})
+			.sort({_id: -1})
+			.addCreatedAt()
+			.contentToHtml()
+			.exec();
+	},
+
+	// 根据文章ID给PV加1
+	incPv: function incPv(postId) {
+		return Post
+			.update({_id: postId}, { $inc: {pv: 1}})
+			.exec();
+	}
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var PostModel = require('../models/posts');
 // 文章页路由
 // 权限检测
 var checkLogin = require('../middlewares/check').checkLogin;
 
 router.get('/', function (req, res, next) {
 	// res.send(req.flash());
-	res.render('posts');
+	// res.render('posts');
+	var author = req.query.author;
+
+	PostModel.getPosts(author)
+		.then(function(posts) {
+			res.render('posts', {
+				posts: posts
+			});
+		})
+		.catch(next);
+
 });
 
 // POST /posts 发表文章
 router.post('/', checkLogin, function (req, res, next) {
-	res.send(req.flash());
+	// res.send(req.flash());
+	var author = req.session.user._id;
+	var title = req.fields.title;
+	var content = req.fields.content;
+	// post参数
+	var post = {
+		author: author,
+		title: title,
+		content: content,
+		pv: 0
+	};
+
+	PostModel.create(post)
+		.then((result) => {
+			post = result.ops[0];
+			req.flash('success', '发表成功');
+			// 发表后跳至文章页
+			res.redirect(`/posts/${post._id}`);
+		})
+		.catch(next);
 })
 
 // GET /posts/create 创建文章页
 router.get('/create', checkLogin, function (req, res, next) {
-	res.send(req.flash());
+	// res.send(req.flash());
+	res.render('create');
 })
 
 // GET /posts/:postId 单独一篇文章
